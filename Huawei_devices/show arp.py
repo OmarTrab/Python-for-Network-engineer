b353be91b48e092d02ce926f3695a9af2ccf143b
om211 <76888946+om211@users.noreply.github.com>
2021-08-14 14:40:55 +0200

    Update show arp.py

--- Huawei_devices/show arp.py
+++ Huawei_devices/show arp.py
@@ -9,12 +9,14 @@ device ={
     'port':'22'
 }                                    #We created a dictionary where we specify the parameters of the device
 
-'''
 
+
+'''
 Let's assign the ConnectionHandler parameter to the ssh_connect
 where we go to indicate the device we have previoulsy listed
 '''
 ssh_coonect = ConnectHandler(**device)
+
 
 '''
 Let's nested the ssh_connect into  the output variable where 
@@ -22,6 +24,7 @@ we utilize the .sendcommand function
 '''
 output = ssh_coonect.send_command('display arp')
 print(output)                                          #show the output result
+
 
 ssh_coonect.disconnect()                   #disconetting from device
 
