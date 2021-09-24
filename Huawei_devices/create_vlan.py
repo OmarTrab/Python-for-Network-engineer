8b332fef7103fee555808b2c0a35ce5985afc2f3
om211 <76888946+om211@users.noreply.github.com>
2021-09-24 12:35:11 +0200

    Update create_vlan.py

--- Huawei_devices/create_vlan.py
+++ Huawei_devices/create_vlan.py
@@ -13,17 +13,17 @@ def config_mode (self,config_command='system-view'):
     return config_mode()
   
 commands = ['vlan batch 25 26 27 28 29 30',
-            'interface vlanif 25'
+            'vlan 25'
             'description prova'
-            'interface vlanif 26',
+            'vlan 26',
             'description Vlan prova1',
-            'interface vlanif 27',
+            'vlan 27',
             'description prova2',
-            'interface vlanif 28',
+            'vlan 28',
             'description prova3',
-            'interface vlanif 29',
+            'vlan 29',
             'description prova4',
-            'interface vlanif 30',
+            'vlan 30',
             'description prova5']
 output = connect.send_config_set(commands)
 
